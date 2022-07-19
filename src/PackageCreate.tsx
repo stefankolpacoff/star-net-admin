ceae76a2c4045c3b62b805b16bf0652f2aab5cc4
AntsoTech <antso-rakotoson@hotmail.fr>
2022-07-19 14:16:09 +0200

    finalized package routes

--- src/PackageCreate.tsx
+++ src/PackageCreate.tsx
@@ -16,14 +16,8 @@ import {
   
   import { PostEditActions } from "./PostEditActions";
   
-  const validateTitle: Validator[] = [required(), minLength(2), maxLength(80)];
-  const validateMainImage: Validator[] = [
-    required(),
-    minLength(2),
-    maxLength(255),
-  ];
-  const validateIdUser: Validator[] = [required()];
-  const validateMainContent: Validator[] = [required(), minLength(2)];
+  const validateName: Validator[] = [required(), minLength(2), maxLength(80)];
+  const validateDescription: Validator[] = [required(), minLength(2)];
   
   export default interface IUser {
     firstName: string;
@@ -35,18 +29,8 @@ import {
   export const PackageCreate = (props: ListProps) => (
     <Create title='Ajouter un article' actions={<PostEditActions />} {...props}>
       <SimpleForm warnWhenUnsavedChanges>
-        <TextInput label='Title' source='title' validate={validateTitle} />
-        <TextInput
-          label='Image'
-          source='mainImage'
-          validate={validateMainImage}
-          fullWidth
-        />
-        <MarkdownInput source='mainContent' validate={validateMainContent} />
-        <ReferenceInput label='Author' source='idUser' reference='users'>
-          {/* Ceci permet de faire une liste déroulante qui va aller afficher le résultat de la fonction optionRenderer : firstname lastname */}
-          <SelectInput optionText={optionRenderer} />
-        </ReferenceInput>
+        <TextInput label='name' source='name' validate={validateName} fullWidth/>
+        <MarkdownInput source='description' validate={validateDescription} />
       </SimpleForm>
     </Create>
   );
--- src/PackageEdit.tsx
+++ src/PackageEdit.tsx
@@ -11,20 +11,14 @@ import {
   } from "react-admin";
   import { PostEditActions } from "./PostEditActions";
   
-//   export default interface IUser {
-//     firstName: string;
-//     lastName: string;
-//   }
-  
-//   const optionRenderer = (user: IUser) => `${user.firstName} ${user.lastName}`;
   
   export const PackageEdit = (props: ListProps) => (
     <Edit {...props} actions={<PostEditActions />}>
       {/* Rajoute des boutons personnalisés dans l'écran d'ajout */}
       <SimpleForm>
         <NumberInput source='id' disabled />
-        <TextInput source='name' />
-        <TextInput source='description' />
+        <TextInput source='name' fullWidth/>
+        <TextInput source='description' fullWidth multiline/>
       </SimpleForm>
     </Edit>
   );
